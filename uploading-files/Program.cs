126e0f3a30bddef8bd243d9868a90aad9da8879c
PhongNguyen2017 <phongnguyen1605@gmail.com>
2017-02-22 23:12:15 +0700

    First Commit - DemoUpload

---
+++ uploading-files/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v2;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using Google.Apis.Drive.v2.Data;
+using System.Collections.Generic;
+
+
+
+
+namespace uploading_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scopes for use with the Google Drive API
+            string[] scopes = new string[] { DriveService.Scope.Drive,
+                                 DriveService.Scope.DriveFile};
+            // có thể thay đổi clientId, clientSecret phù hợp với User hiện hành.
+            var clientId = "843806723358 - j8953j4q34musjjblefvt0aq17ta8lgv.apps.googleusercontent.com";      // From https://console.developers.google.com
+            var clientSecret = "QF8fPma3zEFFYQZknXM_nbYf";          // From https://console.developers.google.com
+            // here is where we Request the user to give us access, or use the Refresh Token that was previously stored in %AppData%
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            },
+                                                                        scopes,
+                                                                        Environment.UserName,
+                                                                        CancellationToken.None,
+                                                                        new FileDataStore("Daimto.GoogleDrive.Auth.Store")).Result;
+
+            
+            //createDirectory(service, title, des, parent);
+            //GetMimeType(tên file);                        định dạng file cần upload
+            //uploadFile(service, filename, parent);        DriveService service chứa thông tin (clientId, clientSecret)
+            
+
+        }
+
+        //// 
+
+        /// Create a new Directory.
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// 
+
+        /// a Valid authenticated DriveService
+        /// The title of the file. Used to identify file or folder name.
+        /// A short description of the file.
+        /// Collection of parent folders which contain this file. 
+        ///                       Setting this field will put the file in all of the provided folders. root folder.
+        /// 
+        public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+        {
+
+            File NewDirectory = null;
+
+            // Create metaData for a new Directory
+            File body = new File();
+            body.Title = _title;
+            body.Description = _description;
+            body.MimeType = "application/vnd.google-apps.folder";
+            body.Parents = new List(  new ParentReference() { Id = _parent } );
+            try
+            {
+                FilesResource.InsertRequest request = _service.Files.Insert(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return NewDirectory;
+        }
+
+        // tries to figure out the mime type of the file.
+        private static string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        /// 
+
+        /// Uploads a file
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// 
+
+        /// a Valid authenticated DriveService
+        /// path to the file to upload
+        /// Collection of parent folders which contain this file. 
+        ///                       Setting this field will put the file in all of the provided folders. root folder.
+        /// If upload succeeded returns the File resource of the uploaded file 
+        ///          If the upload fails returns null
+        public static File uploadFile(DriveService _service, string _uploadFile, string _parent)
+        {
+
+            if (System.IO.File.Exists(_uploadFile))
+            {
+                File body = new File();
+                body.Title = System.IO.Path.GetFileName(_uploadFile);
+                body.Description = "Demo file uploaded by PhongNguyen";
+                body.MimeType = GetMimeType(_uploadFile);
+                body.Parents = new List() { new ParentReference() { Id = _parent } };
+
+                // File's content.
+                byte[] byteArray = System.IO.File.ReadAllBytes(_uploadFile);
+                System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+                try
+                {
+                    FilesResource.InsertMediaUpload request = _service.Files.Insert(body, stream, GetMimeType(_uploadFile));
+                    request.Upload();
+                    return request.ResponseBody;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    return null;
+                }
+            }
+            else
+            {
+                Console.WriteLine("File does not exist: " + _uploadFile);
+                return null;
+            }
+
+        }
+    }
+}
+
